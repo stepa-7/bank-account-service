ee8bf3c0358e5c4945a48ba2deccfbf89065601d
Stepan <sachumovitskii@edu.hse.ru>
2025-11-09 19:50:17 +0300

    Initial commit

--- src/main/java/com/stepa7/bank/BankApplication.java
+++ src/main/java/com/stepa7/bank/BankApplication.java
@@ -1,13 +1,13 @@
-package com.stepa7.demoproject;
+package com.stepa7.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoProjectApplication {
+public class BankApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoProjectApplication.class, args);
+        SpringApplication.run(BankApplication.class, args);
     }
 
 }
